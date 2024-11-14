c4173516fba31c4327670c98791a1bcb4bc81f4e
averyden <igo408gt@gmail.com>
2024-11-14 14:06:45 +0100

    Implement the solver class, only containing the method for solving

---
+++ SodukoSolver/dependencies/solver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodukoSolver.dependencies
+{
+    public class solver
+    {
+
+        public int[,] solve(int[,] grid)
+        {
+            int[] xRow = new int[grid.GetLength(0)];
+            int[] yRow = new int[grid.GetLength(1)];
+
+            for (int x = 0; x < xRow.Length; x++ )
+            {
+                for (int y = 0; y< yRow.Length; y++)
+                {
+                    if (grid[x, y] == 0)
+                    {
+                        for (int curNum = 1; curNum<9; curNum++)
+                        {
+                            if (grid[x,y] == curNum) 
+                            {
+                                Debug.WriteLine("invalid num for placement, moving on."); // increment number as its invalid.
+                            } else
+                            {
+                                grid[x,y] = curNum;
+                                Debug.WriteLine("placed valid num");
+                                if (solve(grid) != null)
+                                {
+                                    return grid; // If the grid is solved, return it
+                                }
+                            }
+                            return null;
+                        }
+                    }
+                }
+            }
+            return grid;
+        } 
+    }
+}
