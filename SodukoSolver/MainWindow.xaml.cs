3d10878acf54c7db6fc2a8922e9f86d54503fb8a
averyden <igo408gt@gmail.com>
2024-11-14 14:27:56 +0100

    Set up the testing code-behind with a pre-set table

--- SodukoSolver/MainWindow.xaml.cs
+++ SodukoSolver/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
+﻿using SodukoSolver.dependencies;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SodukoSolver
 {
@@ -16,9 +8,33 @@ namespace SodukoSolver
     /// </summary>
     public partial class MainWindow : Window
     {
+        Solver s = new Solver();
+
+
+        int[,] SodGrid = { { } };
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = s;
+
+            // Initialize test cells
+            s.Cell00 = 1;
+            s.Cell10 = 3;
+            s.Cell12 = 2;
+            s.Cell21 = 3;
+
+            SodGrid = new int[3, 3]
+            {
+                { s.Cell00, s.Cell01, s.Cell02 },
+                { s.Cell10, s.Cell11, s.Cell12 },
+                { s.Cell21, s.Cell22, s.Cell22 }
+            };
+        }
+
+        private void SolveButton_Click(object sender, RoutedEventArgs e)
+        {
+            s.solve(SodGrid); // find a way to databing all the current cells into one int[,] type.
         }
     }
 }
